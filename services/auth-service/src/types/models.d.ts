f3e15957004aa07181c54d6c6fca05f95ae02790
Geetesh <geetesh472@gmail.com>
2025-11-27 02:24:08 +0530

    job-service added

--- services/auth-service/src/types/models.d.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Document } from "mongoose";
-
-export interface IAuthUser extends Document {
-  name: string;
-  email: string;
-  password: string;
-  role: "super_admin" | "admin" | "student";
-}
