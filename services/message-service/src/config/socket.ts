75b6f280a5b7c514b21298ae0a3ab964086678dc
Geetesh <geetesh472@gmail.com>
2025-11-28 15:22:13 +0530

    removed map

--- services/message-service/src/config/socket.ts
+++ services/message-service/src/config/socket.ts
@@ -2,16 +2,8 @@ import { Server, Socket } from "socket.io";
 import { Message } from "../models/message.model.js";
 
 export const socketHandler = (io: Server) => {
-  const onlineUsers: Map<string, string> = new Map();
-
   io.on("connection", (socket: Socket) => {
     console.log("User connected:", socket.id);
-
-    socket.on("join", (userId: string) => {
-      if (!userId) return;
-      onlineUsers.set(userId, socket.id);
-    });
-
     socket.on("sendMessage", async ({
         userId,
         username,
@@ -41,14 +33,6 @@ export const socketHandler = (io: Server) => {
     );
 
     socket.on("disconnect", (reason) => {
-      for (let [userId, socketId] of onlineUsers.entries()) {
-        if (socketId === socket.id) {
-          onlineUsers.delete(userId);
-          break;
-        }
-      }
-
-      io.emit("onlineUsers", Array.from(onlineUsers.keys()));
       console.log("User disconnected:", socket.id, reason);
     });
   });
