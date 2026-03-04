2ef160f921ff83594b7ae64cbfc3f6ad52fd2d08
Geetesh Dalal <geetesh472@gmail.com>
2026-03-04 17:05:48 +0530

    added cookie support in middlware

--- services/job-service/src/middlewares/checkAuth.middleware.js
+++ services/job-service/src/middlewares/checkAuth.middleware.js
@@ -7,10 +7,16 @@ export const checkAuth = async (req, res, next) => {
         if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
             token = req.headers.authorization.split(' ')[1];
         }
+
+        if(!token && req.cookies?.token) {
+            token = req.cookies.token;
+        }
+
         if(!token) return res.status(401).json({ message: "Unauthorized: Token missing" });
 
         const decoded = jwt.verify(token, env.JWT_SECRET);
         req.user = decoded;
+        
         next();
     } catch (err) {
         console.error("Error validating token: ", err);
