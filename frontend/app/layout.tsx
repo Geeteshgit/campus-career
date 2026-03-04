de446a1561e5718459e821124ece133d9acf4493
Geetesh Dalal <geetesh472@gmail.com>
2026-03-05 02:15:56 +0530

    grouped providers

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import StoreProvider from "./provider";
+import StoreProvider from "../providers/ReduxProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
