21963ec162df3998b97d71000685ead72dbc636c
Geetesh Dalal <geetesh472@gmail.com>
2026-03-06 01:39:39 +0530

    changed api routes

--- services/job-service/src/routes/application.route.js
+++ services/job-service/src/routes/application.route.js
@@ -10,7 +10,7 @@ import { restrictTo } from "../middlewares/restrictTo.middleware.js";
 
 const router = Router();
 
-router.get("/my", checkAuth, restrictTo("student"), getMyApplications);
+router.get("/", checkAuth, restrictTo("student"), getMyApplications);
 router.get("/stats", checkAuth, restrictTo("admin", "super_admin"), getApplicationStats);
 router.get("/:jobId", checkAuth, restrictTo("admin", "super_admin"), getApplicantsForJob);
 router.post("/:jobId", checkAuth, restrictTo("student"), applyToJob);
