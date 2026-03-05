a1995aa1ca4ee7acc344d0af006b52e7fd208b1d
Geetesh Dalal <geetesh472@gmail.com>
2026-03-05 23:57:30 +0530

    removed get user route

--- services/user-service/src/controllers/user.controller.js
+++ services/user-service/src/controllers/user.controller.js
@@ -1,28 +1,4 @@
 import { User } from "../models/user.model.js";
-import { env } from "../config/env.js";
-
-export const getMyAccount = async (req, res) => {
-  try {
-    const userId = req.user.id;
-    if (!userId) {
-      return res.status(400).json({ message: "User ID missing in request" });
-    }
-
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ message: "User Not Found" });
-    }
-
-    return res.status(200).json({
-      message: "Fetched profile",
-      user,
-    });
-  } catch (err) {
-    console.error("Error Fetching My Profile: ", err);
-    return res.status(500).json({ message: "Failed to fetch profile" });
-  }
-};
 
 export const getUserStats = async (req, res) => {
   try {
@@ -58,5 +34,3 @@ export const updateMyAccount = async (req, res) => {
     return res.status(500).json({ message: "Failed to update profile" });
   }
 };
-
-
--- services/user-service/src/routes/user.route.js
+++ services/user-service/src/routes/user.route.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { getMyAccount, getUserStats, updateMyAccount } from "../controllers/user.controller.js";
+import { getUserStats, updateMyAccount } from "../controllers/user.controller.js";
 import { checkAuth } from "../middlewares/checkAuth.middleware.js";
 import { restrictTo } from "../middlewares/restrictTo.middleware.js";
 const router = express.Router();
 
-router.get("/", checkAuth, getMyAccount);
-router.get("/stats", checkAuth, restrictTo("admin", "super_admin"), getUserStats);
 router.put("/", checkAuth, updateMyAccount);
+router.get("/stats", checkAuth, restrictTo("admin", "super_admin"), getUserStats);
 
 export default router;
