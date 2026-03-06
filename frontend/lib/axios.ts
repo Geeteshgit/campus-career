64286dd1fcd5dfee4e3fc06a8bce57cf1b265865
Geetesh Dalal <geetesh472@gmail.com>
2026-03-07 00:08:22 +0530

    added /api to base url

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -2,26 +2,26 @@ import { env } from "@/config/env";
 import axios from "axios";
 
 export const usersApi = axios.create({
-  baseURL: env.USER_SERVICE,
+  baseURL: env.USER_SERVICE + "/api",
   withCredentials: true,
 });
 
 export const jobsApi = axios.create({
-  baseURL: env.JOB_SERVICE,
+  baseURL: env.JOB_SERVICE + "/api",
   withCredentials: true,
 });
 
 export const messagesApi = axios.create({
-  baseURL: env.MESSAGE_SERVICE,
+  baseURL: env.MESSAGE_SERVICE + "/api",
   withCredentials: true,
 });
 
 export const academicsApi = axios.create({
-  baseURL: env.ACADEMIC_CONFIG_SERVICE,
+  baseURL: env.ACADEMIC_CONFIG_SERVICE + "/api",
   withCredentials: true,
 });
 
 export const analyticsApi = axios.create({
-  baseURL: env.ANALYTICS_SERVICE,
+  baseURL: env.ANALYTICS_SERVICE + "/api",
   withCredentials: true,
 });
