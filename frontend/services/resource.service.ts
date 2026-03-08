16850d9207f8d59b02c0ce6c19ab8ba5c5395c72
Geetesh Dalal <geetesh472@gmail.com>
2026-03-08 11:46:38 +0530

    changed update resource arguments

--- frontend/services/resource.service.ts
+++ frontend/services/resource.service.ts
@@ -6,7 +6,9 @@ export const getResources = async () => {
 };
 
 export const getStudentResources = async (studentProgram: string) => {
-  const { data } = await academicsApi.get(`/resources/student?program=${studentProgram}`);
+  const { data } = await academicsApi.get(
+    `/resources/student?program=${studentProgram}`,
+  );
   return data;
 };
 
@@ -19,7 +21,13 @@ export const createResource = async (payload: {
   return data;
 };
 
-export const updateResource = async (id: string, payload: any) => {
+export const updateResource = async ({
+  id,
+  payload,
+}: {
+  id: string;
+  payload: any;
+}) => {
   const { data } = await academicsApi.put(`/resources/${id}`, payload);
   return data;
 };
