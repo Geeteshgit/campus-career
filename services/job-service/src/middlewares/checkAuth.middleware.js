5f174ddc5bc644e76bcd73c595aa594fc418873b
Geetesh Dalal <geetesh472@gmail.com>
2026-03-17 03:43:05 +0530

    changed middleware

--- services/job-service/src/middlewares/checkAuth.middleware.js
+++ services/job-service/src/middlewares/checkAuth.middleware.js
@@ -2,24 +2,20 @@ import jwt from "jsonwebtoken";
 import { env } from "../config/env.js";
 
 export const checkAuth = async (req, res, next) => {
-    try {
-        let token;
-        if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-            token = req.headers.authorization.split(' ')[1];
-        }
+  try {
+    const token = req.headers.authorization?.startsWith("Bearer ")
+      ? req.headers.authorization.split(" ")[1]
+      : req.cookies?.token;
 
-        if(!token && req.cookies?.token) {
-            token = req.cookies.token;
-        }
+    if (!token)
+      return res.status(401).json({ message: "Unauthorized: Token missing" });
 
-        if(!token) return res.status(401).json({ message: "Unauthorized: Token missing" });
+    const decoded = jwt.verify(token, env.JWT_SECRET);
+    req.user = decoded;
 
-        const decoded = jwt.verify(token, env.JWT_SECRET);
-        req.user = decoded;
-        
-        next();
-    } catch (err) {
-        console.error("Error validating token: ", err);
-        return res.status(401).json({ message: "Unauthorized: Invalid token" });
-    }
-}
+    next();
+  } catch (err) {
+    console.error("Error validating token: ", err);
+    return res.status(401).json({ message: "Unauthorized: Invalid token" });
+  }
+};
