6785e8e5610ba352922fc7ada89fdf1d9db958ab
Geetesh Dalal <geetesh472@gmail.com>
2026-03-20 14:33:40 +0530

    added TextArea component

---
+++ frontend/shared/ui/TextArea.tsx
@@ -0,0 +1,24 @@
+// React
+import React, { TextareaHTMLAttributes } from "react";
+
+// External Libraries
+import clsx from "clsx";
+
+type TextAreaProps = {
+  rows?: number;
+} & TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const TextArea = ({ rows = 4, className, ...props }: TextAreaProps): React.JSX.Element => {
+  return (
+    <textarea
+      rows={rows}
+      className={clsx(
+        "px-4 py-2 border border-neutral-300 rounded-lg text-neutral-800 focus:ring-2 focus:ring-blue-500 outline-none w-full",
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+export default TextArea;
